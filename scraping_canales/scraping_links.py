2b9d6fe22e3eed70a664e9f1560b98cf084266f2
Fabian-Martinez-Rincon <fabianmartinezrincon.123@gmail.com>
2024-08-19 13:18:07 -0300

    Agregando algunas configuraciones

--- scraping_canales/scraping_links.py
+++ scraping_canales/scraping_links.py
@@ -1,17 +1,22 @@
-import json
-import os
+
 from collections import defaultdict
 from selenium.webdriver.common.by import By
 from scraping_peliculas_series.config.driver import get_driver
 from scraping_peliculas_series.utils.scraping_utils import click_button, find_element_with_retries
+from scraping_peliculas_series.utils.utils_json import save_to_json
+
+CONFIGURATIONS_BUTTONS = {
+        'guide_button': "//div/button/span/span",
+        'section_scroll': "//section/div[3]/div/div[2]/div/div[1]/div/h3",
+        'categories_file': "resultados.json",
+}
 
 def main():
     driver = get_driver()
     driver.get('https://pluto.tv')
 
-    click_button(driver, "XPATH", '/html/body/div[1]/div/div/div/div[1]/div/button/span/span')
-    click_button(driver, "XPATH", '/html/body/div[1]/div/div/div/main/div[2]/div/div[2]/section/div[3]/div/div[2]/div/div[1]/div/h3')
-
+    click_button(driver, "XPATH", CONFIGURATIONS_BUTTONS['guide_button'])
+    click_button(driver, "XPATH", CONFIGURATIONS_BUTTONS['section_scroll'])
 
     results = defaultdict(list)
     current_tematica = None
@@ -25,7 +30,7 @@ def main():
                 link_element = element_to_click.find_element(By.TAG_NAME, 'a')
                 href_value = link_element.get_attribute('href')
                 timeline_links = element_to_click.find_elements(By.CSS_SELECTOR, '.timelines a')
-                
+
                 resultado = []
                 for index,link in enumerate(timeline_links):
                     href_value2 = link.get_attribute('href')
@@ -34,7 +39,7 @@ def main():
                         "link": href_value2
                     }
                     resultado.append(datos)
-                    
+
                 href_value = href_value.replace("https://pluto.tv/live-tv/", "https://pluto.tv/latam/live-tv/") + "?lang=en"
                 if current_tematica:
                     results[current_tematica].append({
@@ -56,11 +61,7 @@ def main():
             print(f"Después de 3 intentos, no se pudo encontrar el elemento con aria-rowindex='{i}'.")
             break
 
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(current_directory, 'resultados.json')
-    with open(file_path, 'w', encoding='utf-8') as json_file:
-        json.dump(results, json_file, ensure_ascii=False, indent=4)
-
+    save_to_json(results, CONFIGURATIONS_BUTTONS['categories_file'])
     driver.quit()
 
 if __name__ == "__main__":
