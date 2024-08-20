0449b2e83d49a0bcdde4c43a71559442ef7217ad
Fabian-Martinez-Rincon <fabianmartinezrincon.123@gmail.com>
2024-08-19 21:52:00 -0300

    Main completo

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import asyncio
 from scraping_peliculas_series.main import scrape_category_peliculas_series
 from scraping_peliculas_series.scraping import scrape_peliculas_series
+from scraping_canales.scraping_links import scrape_canales
+from scraping_canales.scraping import scrape_data_canales
+
 
 def main():
-    #print("Iniciando scraping de canales...")
-    #scrape_canales()  # Ejecuta el script para scraping de canales
-
+    print("Iniciando scraping de canales...")
+    scrape_canales()
+    asyncio.run(scrape_data_canales())
     print("Iniciando scraping de películas y series...")
-    scrape_category_peliculas_series()  # Ejecuta el script para scraping de películas y series
-    asyncio.run(scrape_peliculas_series())  # Ejecuta el script para scraping de películas y series
-
+    scrape_category_peliculas_series()
+    asyncio.run(scrape_peliculas_series())
     print("Proceso completado.")
 
 if __name__ == "__main__":
